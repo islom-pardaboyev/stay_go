48dd301051b47cd262b8284010fedb8b8ff2e1e2
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-11-06 19:01:09 +0500

    init

--- src/components/swiper/SwiperSection.tsx
+++ src/components/swiper/SwiperSection.tsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import HelloWorldImg from "../../assets/600x400.svg";
 import "swiper/swiper-bundle.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
@@ -12,7 +13,6 @@ export default function SwiperSection() {
           delay: 2500,
           disableOnInteraction: false,
         }}
-        
         modules={[Autoplay, Pagination, Navigation]}
         className="h-[70vh]"
       >
@@ -22,7 +22,12 @@ export default function SwiperSection() {
             .map((_, inx) => (
               <SwiperSlide key={inx} className="w-full px-2">
                 <a href="">
-                <img src={'https://placehold.co/600x400?text=Hello+World'} className="w-full h-[70vh] rounded-xl object-cover" alt="" />
+                  <img
+                    onError={(e) => (e.currentTarget.src = HelloWorldImg)}
+                    src={"https://placehold.co/600x400?text=Hello+World"}
+                    className="w-full h-[70vh] rounded-xl object-cover"
+                    alt=""
+                  />
                 </a>
               </SwiperSlide>
             )),
